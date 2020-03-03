faf90c3ab9c153cff9286cc6223a0f7d480707f1
jkpkonline <jkpkonline@users.noreply.github.com>
2020-03-03 22:25:13 +0000

    1

---
+++ src/test/java/com/jk/spring/servicediscoveryservereureka/ServiceDiscoveryServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.spring.servicediscoveryservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
